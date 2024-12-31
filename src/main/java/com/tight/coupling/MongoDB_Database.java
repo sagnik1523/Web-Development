46b96554402b918b5e51d2f62f2f8aada7e31c52
sagnik1523 <sagnik1523@gmail.com>
2024-12-31 11:38:52 +0530

    added MongoDB class and demonstrated tight coupling i.e. have to make changes in UserManager

---
+++ src/main/java/com/tight/coupling/MongoDB_Database.java
@@ -0,0 +1,8 @@
+package com.tight.coupling;
+
+public class MongoDB_Database {
+    public String getUserDetails() {
+        // Code to fetch user details
+        return "Dummy MongoDB details";
+    }
+}
--- src/main/java/com/tight/coupling/UserManager.java
+++ src/main/java/com/tight/coupling/UserManager.java
@@ -3,7 +3,13 @@ package com.tight.coupling;
 public class UserManager {
     // Currently working in MySQL DB
     MySQL_Database database = new MySQL_Database();
+
+    // Now want to change to Mongo DB
+    MongoDB_Database newDatabase = new MongoDB_Database();
     public String getUserInformation() {
-        return database.getUserDetails();
+        // return database.getUserDetails();
+        // need to change this line also for MongoDB
+
+        return newDatabase.getUserDetails();
     }
 }
